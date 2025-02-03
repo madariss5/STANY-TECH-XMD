9aa20aa65ae8ae3eded9f56b5b434f3b47b2dd50
Stanley Masanyiwa <priscajohnson976@gmail.com>
2025-02-03 13:38:12 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -51,7 +51,7 @@ const date = moment().format('DD/MM/YYYY');
         menuMsg += `*╭────❂* *${cat}* *❂*`;
         for (const cmd of coms[cat]) {
             menuMsg += `  
-*🛑➻* ${cmd}`;
+*🛑* ${cmd}`;
         }
         menuMsg += `
 *╰═════════════❂* \n`

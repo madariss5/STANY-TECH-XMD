674db507b7fa49b8e33c21abf0701a354ae8f431
Stanley Masanyiwa <priscajohnson976@gmail.com>
2025-02-06 19:24:00 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "test", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "ping", reaction: "🤔", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*⭕𝚂𝚃𝙰𝙽𝚈 𝚃𝙴𝙲𝙷 𝚇𝙼𝙳 𝙸𝚂 𝙾𝙽𝙻𝙸𝙽𝙴⭕* 🙏 \n\n ' + "𝚃𝙷𝙴 𝙱𝙾𝚃 𝙲𝙰𝙽𝚃 𝚂𝙻𝙴𝙴𝙿⏰⭕";
-    let d = '                                                                           𝗛𝗘𝗔𝗟𝗧𝗛 𝗦𝗧𝗔𝗧𝗨𝗦✨';
+    let z = '*KHAI NIPO ONLINE MKUU STANY-TECH-XMD HAIWEZI ZIMA ENJOY BWANA* 🙏 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
+    let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/ce58cf8c538b1496fda33.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
-console.log("mon test");
+console.log("mon ping");
